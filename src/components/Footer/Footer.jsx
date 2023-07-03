3f4df0e3d3015cd728e58044b6f6157b5b067329
Sadaf khan <GFG19190@gfg.geeksforgeeks.org>
2023-07-03 13:10:36 +0530

    styling : Adjusted styling of footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,13 @@ import './Footer.css';
 import gitLogo from '../../assets/github-logo.png';
 import linkedInLogo from '../../assets/linkedin-logo.png';
 import { Link } from 'react-router-dom';
+import { useNotes, usePin } from '../../context';
 export const Footer = () => {
+    const {notesArray} = useNotes();
+    const {pinNotes} = usePin();
+
     return(
-        <div className='footer-container d-flex align-center d-column'>
+        <div className= {`footer-container d-flex align-center d-column ${(notesArray.length + pinNotes.length) < 3 && 'fix-pos'}`}>
              <p> Made By Sadaf</p>
              <div className='d-flex foot-soc align-center'>
                 <Link to='https://github.com/sadafkhan0107' target='_blank'> <img className = 'foot-logo' src={gitLogo} alt='github-logo'/></Link>
